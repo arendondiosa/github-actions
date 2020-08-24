2461b726366db7f1c7f1a73da6e5a9066b423e1f
Alejandro E. Rendon <alejandro@rendon.co>
2020-08-24 11:37:58 -0500

    Updated rep

---
+++ scripts/deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git config --global user.email "alejandro@rendon.co"
+git config --global user.name "Test Commit"
+git add .
+git commit -m 'Update pylint score' --allow-empty
+git push origin master
